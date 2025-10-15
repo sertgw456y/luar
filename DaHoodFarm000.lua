733745c16f95542309f484f9f0c73bd335111af9
sertgw456y <thatlesd@gmail.com>
2025-10-15 17:32:23 -0300

    Update DaHoodFarm000.lua

--- DaHoodFarm000.lua
+++ DaHoodFarm000.lua
@@ -2,12 +2,13 @@
 spawn(function()
 loadstring(game:HttpGet("https://raw.githubusercontent.com/sertgw456y/luar/refs/heads/main/Protected_2441670067955070.lua"))()
 end)
-print("loaded 1")
+print("auto farm")
 
 wait(.25)
 
 spawn(function()
 loadstring(game:HttpGet("https://raw.githubusercontent.com/yuvic123/SKIDO-V3/refs/heads/main/farm"))()
 end) 
-print("loaded 2")
+print("loaded dropped money grabber")
+
 
