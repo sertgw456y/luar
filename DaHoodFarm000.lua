afc7e3ac7a719845c2233d76d9837e8a3a77283e
sertgw456y <thatlesd@gmail.com>
2025-10-15 17:28:37 -0300

    Update DaHoodFarm000.lua

--- DaHoodFarm000.lua
+++ DaHoodFarm000.lua
@@ -1,8 +1,13 @@
-spawn(function()
-loadstring(game:HttpGet("https://raw.githubusercontent.com/yuvic123/SKIDO-V3/refs/heads/main/farm"))()
-end) 
-print("loaded 1")
+
 spawn(function()
 loadstring(game:HttpGet("https://raw.githubusercontent.com/sertgw456y/luar/refs/heads/main/Protected_2441670067955070.lua"))()
 end)
+print("loaded 1")
+
+wait(.25)
+
+spawn(function()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/yuvic123/SKIDO-V3/refs/heads/main/farm"))()
+end) 
 print("loaded 2")
+
